1ade582d23d0b8e8ead003b606754ad99c00fa48
Amen <53493383+UseCondomsKid@users.noreply.github.com>
2023-06-01 21:18:01 +0100

    added vim keymaps

--- markdown-app/src/components/ReactMarkdown.tsx
+++ markdown-app/src/components/ReactMarkdown.tsx
@@ -2,13 +2,14 @@ import CodeMirror from '@uiw/react-codemirror';
 import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
 import { languages } from '@codemirror/language-data';
 import { gruvboxDark } from '@uiw/codemirror-themes-all';
+import { vim } from '@replit/codemirror-vim' 
 
 function ReactMarkdown() {
     return (
         <CodeMirror
             value='# Hello World'
             height="200px"
-            extensions={[markdown({ base: markdownLanguage, codeLanguages: languages })]}
+            extensions={[vim(), markdown({ base: markdownLanguage, codeLanguages: languages })]}
             theme={gruvboxDark}
         />
     )
